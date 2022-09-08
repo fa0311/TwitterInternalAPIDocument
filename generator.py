0f56fa9e36616a00b6ce6d4ef61ae758e15e8c47
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-09-09 06:48:44 +0900

    add github action

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import os
+from github import Github
+from lib.md_generator.md_generator import md_generator
 from lib.twitter import twitter_home
 from lib.js_parser.js_parser import js
 
@@ -16,9 +18,6 @@ from lib.md import (
 )
 
 
-os.makedirs("docs/json", exist_ok=True)
-os.makedirs("docs/markdown", exist_ok=True)
-
 twitter = twitter_home()
 src = twitter.get_main_script_url()
 print(f"src: {src}")
@@ -31,11 +30,66 @@ graphql_output = marge_exports(parsed_list, graphql_output)
 freeze_object_output = get_freeze_object(parsed_list)
 feature_switches_output = get_feature_switches(parsed_list)
 
+if os.environ.get("ENV", "Develop") != "GithubAction":
+    os.makedirs("docs/json", exist_ok=True)
+    os.makedirs("docs/markdown", exist_ok=True)
 
-with open("docs/json/GraphQL.json", "w") as f:
-    json.dump(graphql_output, f, ensure_ascii=False, indent=2)
-with open("docs/json/FreezeObject.json", "w") as f:
-    json.dump(freeze_object_output, f, ensure_ascii=False, indent=2)
+    with open("docs/json/GraphQL.json", "w") as f:
+        json.dump(graphql_output, f, ensure_ascii=False, indent=2)
+    # with open("docs/json/FreezeObject.json", "w") as f:
+    #     json.dump(freeze_object_output, f, ensure_ascii=False, indent=2)
+    gen_md_graphql(graphql_output, feature_switches_output).save(
+        "docs/markdown/GraphQL.md"
+    )
+    # gen_md_freeze_object(freeze_object_output).save("docs/markdown/FreezeObject.md")
 
-gen_md_graphql(graphql_output, feature_switches_output).save("docs/markdown/GraphQL.md")
-gen_md_freeze_object(freeze_object_output).save("docs/markdown/FreezeObject.md")
+else:
+
+    file = {
+        "docs/json/GraphQL.json": json.dumps(
+            graphql_output, ensure_ascii=False, indent=2
+        ),
+        # "docs/json/FreezeObject.json": json.dumps(
+        #     freeze_object_output, ensure_ascii=False, indent=2
+        # ),
+        "docs/markdown/GraphQL.md": gen_md_graphql(
+            graphql_output, feature_switches_output
+        ).output,
+        # "docs/markdown/FreezeObject.md": gen_md_freeze_object(
+        #     freeze_object_output
+        # ).output,
+    }
+
+    g = Github(os.environ["GITHUB_TOKEN"])
+    repo = g.get_repo(os.environ["REPOSITORY"])
+    branch = "action"
+    send_pull_request = False
+
+    for file_name, data in file.items():
+        with open(file_name, "r") as f:
+            old_data = f.read()
+
+        if old_data == data:
+            print(f"No change to {file_name}")
+        else:
+            print(f"Commit to {file_name}")
+            send_pull_request = True
+
+            f = repo.get_contents(file_name, ref=branch)
+            repo.update_file(
+                f.path,
+                message=f"Update {file_name} on {branch} branch",
+                content=data,
+                sha=f.sha,
+                branch=branch,
+            )
+
+    if send_pull_request:
+        body = md_generator()
+        body.h1("update")
+        try:
+            repo.create_pull(
+                title="Update Document", body=body.output, head=branch, base="master"
+            )
+        except:
+            print("A pull request already exists")
