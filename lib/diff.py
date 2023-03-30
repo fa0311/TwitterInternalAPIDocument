e49ddd7cbd1c01156ec1585bd8e7b062b69856ad
ふぁ <yuki@yuki0311.com>
2023-03-31 06:12:06 +0900

    fix deck
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/diff.py
+++ lib/diff.py
@@ -1,4 +1,4 @@
-def diff(new_data, old_data, key_func):
+def diff_list(new_data, old_data, key_func):
     output = {
         "add": [],
         "remove": [],
@@ -15,4 +15,24 @@ def diff(new_data, old_data, key_func):
                 break
         else:
             output["remove"].append(key_func(old))
+    return output
+
+
+def diff_dict(new_data, old_data):
+    output = {
+        "add": [],
+        "remove": [],
+    }
+    for new in new_data.keys():
+        for old in old_data.keys():
+            if new == old:
+                break
+        else:
+            output["add"].append(new)
+    for old in old_data.keys():
+        for new in new_data.keys():
+            if new == old:
+                break
+        else:
+            output["remove"].append(old)
     return output
