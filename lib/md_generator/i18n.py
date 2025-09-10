3c5d5160ea5fee83cd3adc07effac17644a94b9f
Hibi_10000 <Hibi-10000@outlook.jp>
2025-09-10 20:35:19 +0900

    use raw string

--- lib/md_generator/i18n.py
+++ lib/md_generator/i18n.py
@@ -3,12 +3,12 @@ import re
 
 
 def get_i18n(i18n_response: str):
-    reg_script = '{name}\("{id}",({fn}|{quote}{any}{quote})\)[,;}}]'.format(
-        quote="[\"']",
-        name="([a-z])",
-        id="([a-z0-9]{8})",
-        any="([\s\S]*?)",
-        fn="\(function\([aent]\){return([\s\S]*?)}\)",
+    reg_script = r'{name}\("{id}",({fn}|{quote}{any}{quote})\)[,;}}]'.format(
+        quote=r"[\"']",
+        name=r"([a-z])",
+        id=r"([a-z0-9]{8})",
+        any=r"([\s\S]*?)",
+        fn=r"\(function\([aent]\){return([\s\S]*?)}\)",
     )
 
     res_1 = {
@@ -16,9 +16,9 @@ def get_i18n(i18n_response: str):
         for script in re.findall(reg_script, i18n_response)
     }
 
-    reg_script = '{{key:"{id}",get:{fn}}}'.format(
-        id="([a-z0-9]{8})",
-        fn="function\(\){return([\s\S]*?)}",
+    reg_script = r'{{key:"{id}",get:{fn}}}'.format(
+        id=r"([a-z0-9]{8})",
+        fn=r"function\(\){return([\s\S]*?)}",
     )
 
     res_2 = {
@@ -34,60 +34,60 @@ def get_i18n(i18n_response: str):
 
 def replace_ver(script: str):
     a = re.sub(
-        "{name}\.createElement\({any},([^,]*?)\)".format(
-            name="([a-z])",
-            any="([\s\S]*?)",
+        r"{name}\.createElement\({any},([^,]*?)\)".format(
+            name=r"([a-z])",
+            any=r"([\s\S]*?)",
         ),
         r"\3",
         script,
     )
     b = re.sub(
-        "{reg}{join}{name}\({ins}\.{placeholder},{any}(({notname}\({any}\){any})+?)\){join}{reg}".format(
-            reg="(\"|')?",
-            join="(\+)?",
-            name="([a-z])",
-            ins="([aent]|this\.props)",
-            placeholder="([A-Za-z0-9_]+)",
-            any="([^\(\)]*?)",
-            notname="([^\+,][\s\S]|[\+,]?[\"'])",
+        r"{reg}{join}{name}\({ins}\.{placeholder},{any}(({notname}\({any}\){any})+?)\){join}{reg}".format(
+            reg=r"(\"|')?",
+            join=r"(\+)?",
+            name=r"([a-z])",
+            ins=r"([aent]|this\.props)",
+            placeholder=r"([A-Za-z0-9_]+)",
+            any=r"([^\(\)]*?)",
+            notname=r"([^\+,][\s\S]|[\+,]?[\"'])",
         ),
         r"\1\2'('+\4.\5,\6\7+')'\12\13",
         a,
     )
     c = re.sub(
-        "{reg}{join}{name}\({ins}\.{placeholder},{any}\){join}{reg}".format(
-            reg="(\"|')?",
-            join="(\+)?",
-            name="([a-z])",
-            ins="([aent]|this\.props)",
-            placeholder="([A-Za-z0-9_]+)",
-            any="([\s\S]*?)",
+        r"{reg}{join}{name}\({ins}\.{placeholder},{any}\){join}{reg}".format(
+            reg=r"(\"|')?",
+            join=r"(\+)?",
+            name=r"([a-z])",
+            ins=r"([aent]|this\.props)",
+            placeholder=r"([A-Za-z0-9_]+)",
+            any=r"([\s\S]*?)",
         ),
         r"\1\2'('+\4.\5,\6+')'\7\8",
         b,
     )
 
     e = re.sub(
-        "{reg}{join}{ins}\.{placeholder}(?={join}{reg})".format(
-            reg="(\"|')?",
-            join="(\+|^|$|,)",
-            ins="([aent]|this\.props)",
-            placeholder="([A-Za-z0-9_]+)",
+        r"{reg}{join}{ins}\.{placeholder}(?={join}{reg})".format(
+            reg=r"(\"|')?",
+            join=r"(\+|^|$|,)",
+            ins=r"([aent]|this\.props)",
+            placeholder=r"([A-Za-z0-9_]+)",
         ),
         r"\1\2{double_quotation}{\4}{double_quotation}",
         c,
     ).replace("{double_quotation}", '"')
     d = re.sub(
-        "{reg},{reg}(?!$)".format(
-            reg="(\"|')",
+        r"{reg},{reg}(?!$)".format(
+            reg=r"(\"|')",
         ),
         r"\1+','+\2",
         e,
     )
     output = re.sub(
-        "('{any}'|\"{any}\")\+(?={reg})".format(
-            reg="(\"|')",
-            any="([\s\S]*?)",
+        r"('{any}'|\"{any}\")\+(?={reg})".format(
+            reg=r"(\"|')",
+            any=r"([\s\S]*?)",
         ),
         r"\1,",
         d,
